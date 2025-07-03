5671ca0be780dcf1644bd6f5e0d05571a87ef1d2
Glorry Sibomana <glorrysibomana758@gmail.com>
2025-07-04 00:16:33 +0300

    add web preview

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,28 @@ export const metadata: Metadata = {
   title: "Tripesa Agent",
   description: "Manage your agency business on the go",
   icons: [{ rel: "icon", url: "/Tripesa-c.svg" }],
+  openGraph: {
+    title: "Tripesa Agent",
+    description: "Manage your agency business on the go",
+    url: "https://tripesa-agent-website.vercel.app/",
+    siteName: "Tripesa Agent",
+    images: [
+      {
+        url: "/website-landing-page.png",
+        width: 1200,
+        height: 630,
+        alt: "Tripesa Agent Landing Page",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Tripesa Agent",
+    description: "Manage your agency business on the go",
+    images: ["/app-landing-page.png"],
+  },
 };
 
 export default function RootLayout({
